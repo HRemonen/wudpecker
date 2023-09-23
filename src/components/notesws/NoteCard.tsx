a7ce275842f3c9e64b870238b62ffe16cd86e777
Henri Remonen <henri@remonen.fi>
2023-09-23 14:22:04 +0300

    feat: Create Note card component

---
+++ src/components/notesws/NoteCard.tsx
@@ -0,0 +1,22 @@
+import { NoteFields } from '../../types'
+import timeSinceCreation from '../../utils/timedeltas'
+
+const NoteCard = ({ note }: { note: NoteFields }) => {
+  const sinceCreation = timeSinceCreation(note.createdAt)
+
+  return (
+    <div className='block max-w-sm w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-orange-200'>
+      <div>
+        <h5 className='mb-2 text-lg font-semibold tracking-tight text-gray-700'>
+          {note.title}
+        </h5>
+        <p className='font-normal text-gray-400'>{note.content}</p>
+      </div>
+      <div className='mt-4'>
+        <p className='text-gray-400'>{sinceCreation}</p>
+      </div>
+    </div>
+  )
+}
+
+export default NoteCard
