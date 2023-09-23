46124548bfb4400a5995471d4186942c76b3e474
Henri Remonen <henri@remonen.fi>
2023-09-23 14:21:11 +0300

    feat: Create note data and type for the notes, render initial note cards

--- src/components/notesws/NoteWorkspace.tsx
+++ src/components/notesws/NoteWorkspace.tsx
@@ -1,9 +1,11 @@
 import SearchBar from '../common/SearchBar'
+import NoteSelector from './NoteSelector'
 
 const NoteWorkspace = () => (
   <section className='mt-2'>
     <SearchBar placeholder='Search for notes' />
     <h1 className='mt-8 text-3xl font-semibold'>Notes</h1>
+    <NoteSelector />
   </section>
 )
 
--- src/types.ts
+++ src/types.ts
@@ -16,3 +16,32 @@ export interface SearchBarProps {
   placeholder: string
   onChange?: (value: string) => void
 }
+
+export interface ChecklistItem {
+  text: string
+  completed: boolean
+}
+
+export interface NoteFields {
+  title: string
+  content?: string
+  subtitle?: string
+  tags?: string[]
+  checklist?: {
+    title: string
+    items: ChecklistItem[]
+  }
+  collaborators?: {
+    name: string
+    img: ReactNode
+  }[]
+  location?: {
+    name: string
+  }
+  createdAt: string
+  updatedAt: string
+}
+
+export interface Note {
+  items: NoteFields[]
+}
