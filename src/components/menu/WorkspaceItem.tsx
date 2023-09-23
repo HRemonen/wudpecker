7754f308ab85ccffc4963a3f9ac9a922c26b6026
Henri Remonen <henri@remonen.fi>
2023-09-23 12:00:00 +0300

    feat: Create the dots for the selected workspace

--- src/components/menu/WorkspaceItem.tsx
+++ src/components/menu/WorkspaceItem.tsx
@@ -15,6 +15,26 @@ const WorkspaceItem = ({ to, name, icon }: WorkspaceItemProps) => (
           }`}
         >
           {icon} <span className='pl-4 '>{name}</span>
+          {isActive && (
+            <span className='ml-auto'>
+              {' '}
+              {/* Use ml-auto to push this span to the right */}
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+                />
+              </svg>
+            </span>
+          )}
         </span>
       )}
     </NavLink>
